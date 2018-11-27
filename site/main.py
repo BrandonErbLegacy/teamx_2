2d7d5dab8f82b979bfe666f93c580532ce2d4393
Brandon <bme54950@gmail.com>
2018-11-27 09:32:40 -0500

    Added endpoint for registering user, logging in. Also added templates for those actions

--- site/main.py
+++ site/main.py
@@ -2,9 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from threading import Thread
-from flask import Flask, request, render_template
-from hashlib import sha256
-from uuid import uuid4
+from flask import Flask, request, render_template, make_response
 
 import json
 
@@ -25,7 +23,11 @@ main_session = session_maker()
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template('index.html', title='Home')
+	if (request.cookies.get('teamx_session')):
+		user = Session.GetUserBySession(main_session, request.cookies.get('teamx_session'))
+		if (user):
+			return render_template('index.html', title='Home', user=user)
+	return render_template('index.html', title='Home', user=None)
 
 @app.route('/about')
 def about():
@@ -41,7 +43,26 @@ def contact():
 ###############################
 @app.route('/register')
 def user_register():
-	return render_template('register.html', title='Register to become a user')
+	return render_template('register.html', title='Register to become a user', user=None)
+
+@app.route('/welcome')
+def welcome_user():
+	if (request.cookies.get('teamx_session')):
+		user = Session.GetUserBySession(main_session, request.cookies.get('teamx_session'))
+		if (user):
+			return render_template('welcome.html', title='Welcome to Team X', user=user)
+	return render_template('register.html', title='Register to become a user', user=None)
+
+@app.route('/logout')
+def logout():
+	response = make_response(render_template('login.html', title="Please log in", user=None))
+	response.set_cookie('teamx_session', '', expires=0)
+	return response
+
+@app.route('/login')
+def login():
+	return render_template('login.html', title="Please log in", user=None)
+
 
 ###############################
 #####  Admin Site Routes  #####
@@ -49,11 +70,47 @@ def user_register():
 
 @app.route('/admin')
 def admin_login():
-    return "Admin login or redirect"
+	return "Admin login or redirect"
 
 ###############################
 #####   Rest API Routes   #####
 ###############################
+@app.route('/api/authentication/user', methods=['POST'])
+def api_auth_user():
+	username = get_value_or_blank(request, "username")
+	password = get_value_or_blank(request, "password")
+
+	if (username == "" or password == ""):
+		return "Both fields cannot be blank", 403
+	else:
+		session = User.LogUserIn(main_session, username, password, request.remote_addr)
+		if session == None:
+			return "The provided credentials did not match our records", 403
+		else:
+			return session.id, 200
+
+@app.route('/api/create/user', methods=['POST'])
+def api_create_user():
+	try:
+		username = get_value_or_blank(request, "username")
+		password = get_value_or_blank(request, "password")
+		display_name = get_value_or_blank(request, "display_name")
+		first_name = get_value_or_blank(request, "first_name")
+		email = get_value_or_blank(request, "email")
+		user, session_id = User.RegisterUser(main_session, username, password, display_name, first_name, email, request.remote_addr)
+		return session_id, 200
+	except RecordExists as ex:
+		return "Your "+ex.field+" must be unique. That one is already in use", 500
+
+###############################
+#####  Utility Functions  #####
+###############################
+def get_value_or_blank(request, value, type="POST"):
+	#Returns either the value pulled from a request. Or defaults to nothing
+	if (type == "POST"):
+		return "" if request.form.get(value) is None else request.form.get(value)
+	else:
+		raise NotImplemented
 
 DatabaseBase.metadata.create_all(db_engine)
 main_session.commit()
--- site/objects/mainobjects.py
+++ site/objects/mainobjects.py
@@ -5,88 +5,148 @@ from sqlalchemy.ext.declarative import DeclarativeMeta
 from sqlalchemy.orm import load_only
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
+
+from uuid import uuid4
+from hashlib import sha256
 DatabaseBase = declarative_base()
 
+class RecordExists(Exception):
+	def __init__(self, message, field):
+
+		# Call the base class constructor with the parameters it needs
+		super(RecordExists, self).__init__(message)
+
+		self.field = field
+
 class User(DatabaseBase):
-    __tablename__ = "Users"
+	__tablename__ = "Users"
 
-    id = Column(String(36), primary_key=True)
-    username = Column(String)
-    password = Column(String)
-    display_name = Column(String)
-    first_name = Column(String)
-    last_name = Column(String)
-    email = Column(String)
-    salt = Column(String(36))
-    date_registered = Column(DateTime(timezone=True), server_default=func.now())
-    last_seen = Column(DateTime(timezone=True), onupdate=func.now())
-    is_admin = Column(Boolean(), default=False) #Admin privs only
-    is_superuser = Column(Boolean(), default=False) #All admin privs, plus can make admins
+	id = Column(String(36), primary_key=True)
+	username = Column(String)
+	password = Column(String)
+	display_name = Column(String)
+	first_name = Column(String)
+	email = Column(String)
+	salt = Column(String(36))
+	date_registered = Column(DateTime(timezone=True), server_default=func.now())
+	last_seen = Column(DateTime(timezone=True), onupdate=func.now(), server_default=func.now())
+	is_admin = Column(Boolean(), default=False) #Admin privs only
+	is_superuser = Column(Boolean(), default=False) #All admin privs, plus can make admins
 
-	def RegisterUser(username, password, display_name, first_name, last_name, email):
+	def RegisterUser(session, username, password, display_name, first_name, email, ip):
+		users_with_username = session.query(User).filter(User.username == username).first()
+		if (users_with_username != None):
+			raise RecordExists("", "username")
+
+		users_with_email = session.query(User).filter(User.email == email).first()
+		if (users_with_email != None):
+			raise RecordExists("", "password")
+
+		users_with_display_name = session.query(User).filter(User.display_name == display_name).first()
+		if (users_with_display_name != None):
+			raise RecordExists("", "display_name")
+
+		user = User()
+		user.id = new_uuid()
+		user.salt = new_uuid()
+		user.username = username
+		user.password = sha256((user.salt+password).encode("utf-8")).hexdigest()
+		user.display_name = display_name
+		user.first_name = first_name
+		user.email = email
+
+		session.add(user)
+		user_session = Session.CreateSession(session, user, ip)
+
+		return (user, user_session.id)
+
+	def LogUserIn(session, username, password, ip_addr):
+		target_user = session.query(User).filter(User.username == username).first()
+		if (target_user == None):
+			return None
+		hashed_password = sha256((target_user.salt+password).encode("utf-8")).hexdigest()
+		if hashed_password == target_user.password:
+			user_session = Session.CreateSession(session, target_user, ip_addr)
+			return user_session
+		else:
+			return None
+
+	def UpdateUser(session, username, password, display_name, first_name, last_name, email):
 		pass
 
-	def LogUserIn(username, password):
+class Session(DatabaseBase):
+	__tablename__ = "Sessions"
+
+	id = Column(String(36), primary_key=True)
+	user_id = Column(String(36)) #User.id
+	ip_address = Column(String(36))
+	date_registered = Column(DateTime(timezone=True), server_default=func.now())
+	is_expired = Column(Boolean(), default=False)
+	#We'll use is_expired to do soft deletes of sessions (to allow tracking sessions)
+
+	def CreateSession(session, user_object, ip_address):
+		user_session = Session()
+		user_session.id = new_uuid()
+		user_session.user_id = user_object.id
+		user_session.ip_address = ip_address
+
+		session.add(user_session)
+
+		session.commit()
+		return user_session
+
+	def ValidateSession(session, session_object, ip_address):
 		pass
 
-	def UpdateUser(username, password, display_name, first_name, last_name, email):
+	def DeleteSession(session, session_object):
 		pass
 
-class Session(Data)
-    __tablename__ = "Users"
-
-    id = Column(String(36), primary_key=True)
-	user_id = Column(String(36) #User.id
-	#expiration_date =
-
-	def CreateSession(user_object, ip_address):
-		pass
-
-	def ValidateSession(session_object, ip_address):
-		pass
-
-	def DeleteSession(session_object):
-		pass
-
-	def GetUserBySession(session_object):
-		pass
+	def GetUserBySession(session, session_id_or_object):
+		print(session_id_or_object)
+		user_obj = None
+		try:
+			user_obj = session.query(User).filter(User.id == session_id_or_object.user_id).first()
+		except:
+			session_obj = session.query(Session).filter(Session.id == session_id_or_object).first()
+			user_obj = session.query(User).filter(User.id == session_obj.user_id).first()
+		return user_obj
 
 class Server(DatabaseBase):
-    __tablename__ = "Servers"
+	__tablename__ = "Servers"
 
-    id = Column(String(36), primary_key=True)
-    name = Column(String)
-    path_to_pic = Column(String)
-    path_to_technic = Column(String)
-    minecraft_version = Column(String)
+	id = Column(String(36), primary_key=True)
+	name = Column(String)
+	path_to_pic = Column(String)
+	path_to_technic = Column(String)
+	minecraft_version = Column(String)
 
-	def AddServer(name, path_to_pic, path_to_technic, minecraft_version):
+	def AddServer(session, name, path_to_pic, path_to_technic, minecraft_version):
 		pass
 
-	def UpdateServer(name, path_to_pic, path_to_technic, minecraft_version):
+	def UpdateServer(session, name, path_to_pic, path_to_technic, minecraft_version):
 		pass
 
 class ServerPlayerMapping(DatabaseBase):
-    __tablename__ = "MinecraftPlayers"
-    id = Column(String(36), primary_key=True)
-    minecraft_name = Column(String)
-    minecraft_guid = Column(String(36))
-    user_id = Column(String(36)) #User.id
-    server_id = Column(String(36)) #Server.id
+	__tablename__ = "MinecraftPlayers"
+	id = Column(String(36), primary_key=True)
+	minecraft_name = Column(String)
+	minecraft_guid = Column(String(36))
+	user_id = Column(String(36)) #User.id
+	server_id = Column(String(36)) #User.id
 
-	def AddServerPlayerMapping(minecraft_name, minecraft_guid, user_object, server_object):
+	def AddServerPlayerMapping(session, minecraft_name, minecraft_guid, user_object, server_object):
 		pass
 
-	def UpdateServerPlayerMapping(server_player_mapping_object, server_object):
+	def UpdateServerPlayerMapping(session, server_player_mapping_object, server_object):
 		pass
 
 class ServerActivityLog(DatabaseBase):
-    __tablename__ = "ServerActivityLogs"
-    id = Column(String(36), primary_key=True)
-    action = Column(String)
+	__tablename__ = "ServerActivityLogs"
+	id = Column(String(36), primary_key=True)
+	action = Column(String)
 	server_player_mapping_id = Column(String(36)) #ServerPLayerMapping.id
 
-	def AddServerActivityLog(action, server_player_mapping_object=None):
+	def AddServerActivityLog(session, action, server_player_mapping_object=None):
 		pass
 # Actions:
 #  - PlayerLoggedOn
@@ -98,21 +158,21 @@ class ServerActivityLog(DatabaseBase):
 #  - Server Crashed
 
 class Mod(DatabaseBase):
-    __tablename__ = "Mods"
-    id = Column(String(36), primary_key=True)
-    name = Column(String)
-    version = Column(String)
-	server_id = Column(String(36))
+	__tablename__ = "Mods"
+	id = Column(String(36), primary_key=True)
+	name = Column(String)
+	version = Column(String)
+	server_id = Column(String(36)) #User.id
 	def AddMod(name, version, server_object):
 		pass
 
 class Update(DatabaseBase):
-    __tablename__ = "Updates"
-    id = Column(String(36), primary_key=True)
+	__tablename__ = "Updates"
+	id = Column(String(36), primary_key=True)
 	title = Column(String)
 	desc = Column(String)
 	for_server = Column(Boolean(), default=False)
-	server_id = Column(String(36))
+	server_id = Column(String(36)) #User.id
 
 	def AddUpdate(title, desc, for_server, server_object):
 		pass
@@ -133,7 +193,9 @@ class Update(DatabaseBase):
 
 
 
-
+## Utility Functions ##
+def new_uuid():
+	return str(uuid4())
 
 
 
